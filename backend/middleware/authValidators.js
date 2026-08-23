a5ae46404669a732022b7d55061929219972e446
Noah Gullo <noahgulloingram@gmail.com>
2026-08-23 09:32:35 -0700

    feat: add login/signup validation

---
+++ backend/middleware/authValidators.js
@@ -0,0 +1,46 @@
+const { body } = require("express-validator");
+
+const signupValidators = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3, max: 30 })
+    .withMessage("Username must be between 3 and 30 characters"),
+
+  body("email")
+    .trim()
+    .notEmpty()
+    .withMessage("Email is required")
+    .isEmail()
+    .withMessage("Enter a valid email address")
+    .normalizeEmail(),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain an uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain a lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain a number"),
+];
+
+const loginValidators = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required"),
+];
+
+module.exports = {
+  signupValidators,
+  loginValidators,
+}
